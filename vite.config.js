17b2c5b0dd417facf50fb7978682f182095c055d
joalvara13 <joalvara13@gmail.com>
2023-08-11 23:31:34 -0500

    adding comma

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cnct-tools/'
+  base: '/cnct-tools/',
   plugins: [react()],
 })
