2dbb8739ec8962938b6d13ace261f31329c22066
joalvara13 <joalvara13@gmail.com>
2023-08-15 19:49:17 -0500

    adding cnct logo with link

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import AirEstimator from './Air Systems Calculator';
 import './App.css';
 
 function App() {
-  return ( 
-  <div>
-    <h1>Air Systems Calculator</h1>
-    <AirEstimator />
-    
-  </div>
+  return (  
+    <div>
+      <div class="logo-wrapper" data-content-field="site-title">
+        <a href="https://cnct.design/"><img class="logo" src="/public/cnct_v2.png" /></a>
+      </div>
+      
+      <h1>Air Systems Calculator</h1>
+      <AirEstimator />
+    </div>
 )};
 
 export default App;
